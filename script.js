57035a5376f5d1d5a647c4fa99afe80a1f0bcea3
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-21 19:54:49 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+function dragStart(event) {
+    event.dataTransfer.setData("Text", event.target.id);
+  }
+  
+  function allowDrop(event) {
+    event.preventDefault();
+  }
+  
+  function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("Text");
+    var draggedDiv = document.getElementById(data);
+    var droppedDiv = event.target;
+    let temp = draggedDiv.id;
+    draggedDiv.id = droppedDiv.id;
+    droppedDiv.id = temp;
+    console.log(draggedDiv);
+    console.log("**************************************");
+    console.log(droppedDiv);
+  }
